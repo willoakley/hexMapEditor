089999aa35a1538f3a672bf86d98107c2bf57301
willoakley <happyfarm@hotmail.com>
2014-03-12 17:18:20 +0000

    Add draw functions for hex types

--- drawFuncs.js
+++ drawFuncs.js
@@ -1,3 +1,15 @@
+window.colours = {
+	water: "#0066FF",
+	tile: "#D6D6D6",
+	hill: "#AEAEAE",
+	lightWoods: "#339933",
+	heavyWoods: "#003300",
+	hilight: "rgba(255,128,64, 0.5)",
+	building: "#404040",
+	outline: "black",
+	lightOutline: "white",
+};
+
 window.drawFuncs = {
 	_mergeOptions: function (additionalOptions) {
 		var options = {
@@ -9,17 +21,17 @@ window.drawFuncs = {
 			},
 			text: {
 				value: "",
-				colour: "black",
+				colour: window.colours.outline,
 				isIcon: false,
 			},
 			outline: {
 				show: true,
-				colour: "black",
+				colour: window.colours.outline,
 				thickness: 1,
 			},
 			fill: {
 				show: false,
-				colour: "white",
+				colour: window.colours.lightOutline,
 			},
 		};
 
@@ -74,11 +86,19 @@ window.drawFuncs = {
 			return;
 		}
 
-		var colour = "rgba(255,128,64, 0.5)";
-		options.fill = { show: true, colour: colour };
-		options.outline = { show: true, colour: colour, thickness: 2 };
+		options.fill = { show: true, colour: window.colours.hilight };
+		options.outline = { show: true, colour: window.colours.hilight, thickness: 2 };
 
 		window.drawFuncs._hexagon(context, options);
+	},
+
+	_outlineHex: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, offset: pixelLocation,
+			outline: { show: true, colour: window.colours.outline },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
 	},
 
 	hilightHex: function(context, pixelLocation, scale, rotation, state, itemArgs) {
@@ -89,7 +109,7 @@ window.drawFuncs = {
 	tile: function(context, pixelLocation, scale, rotation, state, itemArgs) {
 		var options = {
 			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
-			fill: { show: true, colour: "grey", },
+			fill: { show: true, colour: window.colours.tile, },
 		};
 		window.drawFuncs._hexagon(context, options);
 
@@ -108,7 +128,7 @@ window.drawFuncs = {
 		tempGrid.draw(context);
 
 		options.fill = undefined;
-		options.outline = { show: true, thickness: 2, colour: "black" };
+		options.outline = { show: true, thickness: 2 };
 		window.drawFuncs._hexagon(context, options);
 
 		window.drawFuncs._hilightHexagon(context, options, state);
@@ -121,7 +141,7 @@ window.drawFuncs = {
 		};
 
 		var points = window.drawFuncs._hexagon(context, options);
-		context.fillStyle = "green";
+		context.fillStyle = window.colours.lightWoods;
 		context.textAlign = 'left';
 		context.font = "bold " + Math.floor(options.scale) + "px FontAwesome";
 		context.fillText("\uf18c", points[0].x - (scale * 0.33), points[0].y + (scale * 0.75));
@@ -129,11 +149,127 @@ window.drawFuncs = {
 		window.drawFuncs._hilightHexagon(context, options, state);
 	},
 
+	heavyWoods: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+		};
+
+		var points = window.drawFuncs._hexagon(context, options);
+		context.fillStyle = window.colours.heavyWoods;
+		context.textAlign = 'left';
+		context.font = "bold " + Math.floor(options.scale) + "px FontAwesome";
+		context.fillText("\uf18c", points[0].x - (scale * 0.33), points[0].y + (scale * 0.75));
+
+		context.fillText("\uf18c", points[1].x - (scale * 0.33), points[1].y + (scale * 0.33));
+
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	depthZeroRiver: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.water },
+			text: { value: "D0" },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
 	depthOneRiver: function(context, pixelLocation, scale, rotation, state, itemArgs) {
 		var options = {
 			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
-			fill: { show: true, colour: "cyan" },
-			text: { value: "D1", colour: "black" },
+			fill: { show: true, colour: window.colours.water },
+			text: { value: "D1" },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	depthTwoRiver: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.water },
+			text: { value: "D2" },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightZeroBuilding: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.building },
+			text: { value: "B0", colour: window.colours.lightOutline },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightOneBuilding: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.building },
+			text: { value: "B1", colour: window.colours.lightOutline },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightTwoBuilding: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.building },
+			text: { value: "B2", colour: window.colours.lightOutline },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightThreeBuilding: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.building },
+			text: { value: "B3", colour: window.colours.lightOutline },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightFourBuilding: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.building },
+			text: { value: "B4", colour: window.colours.lightOutline },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightOneHill: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.hill },
+			text: { value: "H1" },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightTwoHill: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.hill },
+			text: { value: "H2" },
+		};
+		window.drawFuncs._hexagon(context, options);
+		window.drawFuncs._hilightHexagon(context, options, state);
+	},
+
+	heightThreeHill: function(context, pixelLocation, scale, rotation, state, itemArgs) {
+		var options = {
+			scale: scale, state: state, offset: pixelLocation, rotation: rotation,
+			fill: { show: true, colour: window.colours.hill },
+			text: { value: "H3" },
 		};
 		window.drawFuncs._hexagon(context, options);
 		window.drawFuncs._hilightHexagon(context, options, state);
